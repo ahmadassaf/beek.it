73b9c4a07478d346a45f5ac8d3081d01111e6e56
Martin Czygan <martin.czygan@gmail.com>
2014-10-11 20:47:36 +0200

    do not duplicate the uglyness

--- jobs.py
+++ jobs.py
@@ -96,6 +96,8 @@ def query_embedly(url):
         es = elasticsearch.Elasticsearch()
         es.update(index='beek', doc_type='page', id=url_to_doc_id(url), body={'doc': {'embedly': data}})
 
+
+
 def get_terms_images():
     """ Try to fetch the depictions of terms from dbpedia and cache the
         URL locally for faster access. """
@@ -104,34 +106,23 @@ def get_terms_images():
         raise RuntimeError('we need /terms')
     terms = json.loads(r.text)
 
+    # store everything we find here
     imagery = {'cities': {}, 'people': {}}
 
-    # for cities
-    for name, url in terms['cities'].iteritems():
-        dbpedia_live_url = '%s.json' % url.replace('dbpedia.org/resource', 'live.dbpedia.org/data')
-        r = requests.get(dbpedia_live_url)
-        if r.status_code >= 400:
-            continue
-        bag = json.loads(r.text)
-        for subject, po in bag.iteritems():
-            for predicate, o in po.iteritems():
-                if predicate == "http://xmlns.com/foaf/0.1/depiction":
-                    for doc in o:
-                        print(name, url, doc['value'])
-                        imagery['cities'][name] = doc['value']
-    # for people
-    for name, url in terms['people'].iteritems():
-        dbpedia_live_url = '%s.json' % url.replace('dbpedia.org/resource', 'live.dbpedia.org/data')
-        r = requests.get(dbpedia_live_url)
-        if r.status_code >= 400:
-            continue
-        bag = json.loads(r.text)
-        for subject, po in bag.iteritems():
-            for predicate, o in po.iteritems():
-                if predicate == "http://xmlns.com/foaf/0.1/depiction":
-                    for doc in o:
-                        print(name, url, doc['value'])
-                        imagery['people'][name] = doc['value']
+    for kind in ('cities', 'people'):
+        for name, url in terms[kind].iteritems():
+            dbpedia_live_url = '%s.json' % url.replace('dbpedia.org/resource', 'live.dbpedia.org/data')
+            r = requests.get(dbpedia_live_url)
+            if r.status_code >= 400:
+                continue
+            bag = json.loads(r.text)
+            for subject, po in bag.iteritems():
+                for predicate, o in po.iteritems():
+                    if predicate == "http://xmlns.com/foaf/0.1/depiction":
+                        for doc in o:
+                            print(name, url, doc['value'])
+                            imagery[kind][name] = doc['value']
 
+    # save results as single document
     es = elasticsearch.Elasticsearch()
     es.index(index='beek', doc_type='images', id='dbpedia', body=imagery, refresh=True)
